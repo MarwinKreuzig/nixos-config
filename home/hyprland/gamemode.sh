59050b7533333ef3701e31ef226ff20ac3e5ce66
Marwin Kreuzig <marwin@kreuzig.info>
2024-04-02 22:14:33 +0200

    improved gamemode

--- home/hyprland/gamemode.sh
+++ home/hyprland/gamemode.sh
@@ -6,7 +6,9 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword animations:enabled 0;\
         keyword decoration:drop_shadow 0;\
         keyword decoration:blur:enabled 0;\
-        keyword decoration:rounding 0"
+        keyword decoration:rounding 0; \
+        keyword decoration:active_opacity 1.0; \
+        keyword decoration:inactive_opacity 1.0;"
     exit
 fi
 hyprctl reload
