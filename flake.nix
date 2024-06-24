3bcce92b1c8d0c275dc67b0f21d1517984ed66f3
Marwin Kreuzig <marwin@kreuzig.info>
2024-06-24 05:41:42 +0200

    removed unused input

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   nixConfig = {
     experimental-features = [ "nix-command" "flakes" ];
     substitutors = [
-      "https://hyprland.cachix.org"
+#      "https://hyprland.cachix.org"
       "https://app.cachix.org/cache/jakestanger"
       "https://cache.nixos.org/"
     ];
@@ -13,7 +13,7 @@
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+#     "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "jakestanger.cachix.org-1:VWJE7AWNe5/KOEvCQRxoE8UsI2Xs2nHULJ7TEjYm7mM="
     ];
@@ -28,7 +28,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nvim-nix-config.url = "github:MarwinKreuzig/nvim-nix-config";
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+ #   hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     hyprland-contrib = {
       url = "github:hyprwm/contrib";
       inputs.nixpkgs.follows = "nixpkgs";
