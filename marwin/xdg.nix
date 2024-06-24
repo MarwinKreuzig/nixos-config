56931038a15a54aa49a168c14b82a3b83a241aa7
Marwin Kreuzig <marwin@kreuzig.info>
2024-06-24 05:41:20 +0200

    tried to get xdg mime associatons to work

--- marwin/xdg.nix
+++ marwin/xdg.nix
@@ -1,6 +1,56 @@
 { config, pkgs, ... }:
 let
   browser = [ "firefox" ];
+  image-viewer = [ "feh" ];
+  associations = builtins.mapAttrs (_: v: (map (e: "${e}.desktop") v)) {
+        "image/gif" = image-viewer;
+        "image/jpeg" = image-viewer;
+        "image/png" = image-viewer;
+        "image/vnd.adobe.photoshop" = [ "krita" ];
+        "image/webp" = image-viewer;
+
+        "video/mp4" = [ "mpv" ];
+        "video/ogg" = [ "mpv" ];
+        "video/webm" = [ "mpv" ];
+        "video/x-flv" = [ "mpv" ];
+        "video/x-matroska" = [ "mpv" ];
+        "video/x-ms-wmv" = [ "mpv" ];
+        "video/x-ogm+ogg" = [ "mpv" ];
+        "video/x-theora+ogg" = [ "mpv" ];
+
+        "audio/aac" = [ "mpv" ];
+        "audio/flac" = [ "mpv" ];
+        "audio/mp4" = [ "mpv" ];
+        "audio/mpeg" = [ "mpv" ];
+        "audio/ogg" = [ "mpv" ];
+        "audio/x-wav" = [ "mpv" ];
+
+        "text/plain" = [ "nvim" ];
+        "text/html" = browser;
+        "text/markdown" = [ "nvim" ];
+        "text/uri-list" = browser;
+
+        "application/x-extension-htm" = browser;
+        "application/x-extension-html" = browser;
+        "application/x-extension-shtml" = browser;
+        "application/x-extension-xht" = browser;
+        "application/x-extension-xhtml" = browser;
+        "application/xhtml+xml" = browser;
+        "x-scheme-handler/about" = browser;
+        "x-scheme-handler/ftp" = browser;
+        "x-scheme-handler/http" = browser;
+        "x-scheme-handler/https" = browser;
+        "x-scheme-handler/unknown" = browser;
+        "x-scheme-handler/chrome" = browser;
+
+        "application/json" = browser;
+        "application/pdf" = browser;
+
+        "inode/directory" = [ "dolphin" ];
+
+        "x-scheme-handler/discord" = [ "discord" ];
+        "x-scheme-handler/jetbrains-gateway" = [ "jetbrains-gateway" ];
+      };
 in
 {
   home.packages = with pkgs; [ xdg-utils ];
@@ -10,27 +60,8 @@ in
     cacheHome = config.home.homeDirectory + "/.local/cache";
     mimeApps = {
       enable = true;
-      defaultApplications = builtins.mapAttrs (_: v: (map (e: "${e}.desktop") v)) {
-        "image/*" = [ "feh" ];
-        "video/*" = [ "mpv" ];
-        "application/x-extension-htm" = browser;
-        "application/x-extension-html" = browser;
-        "application/x-extension-shtml" = browser;
-        "application/x-extension-xht" = browser;
-        "application/x-extension-xhtml" = browser;
-        "application/xhtml+xml" = browser;
-        "text/html" = browser;
-        "x-scheme-handler/about" = browser;
-        "x-scheme-handler/ftp" = browser;
-        "x-scheme-handler/http" = browser;
-        "x-scheme-handler/https" = browser;
-        "x-scheme-handler/unknown" = browser;
-        "application/json" = browser;
-        "application/pdf" = browser;
-        "inode/directory" = [ "nnn" ];
-        "x-scheme-handler/discord" = [ "discord" ];
-        "x-scheme-handler/jetbrains-gateway" = [ "jetbrains-gateway" ];
-      };
+      defaultApplications = associations;
+      associations.added = associations;
     };
 
     userDirs = {
