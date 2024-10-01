5a5f95075ca2229ca1bca22099770b22d0a0a387
Marwin Kreuzig <marwin@kreuzig.info>
2024-10-01 22:52:14 +0200

    fixed xdg portals, maybe

--- hosts/common/xdg.nix
+++ hosts/common/xdg.nix
@@ -3,11 +3,7 @@
   xdg.portal = {
     enable = true;
     xdgOpenUsePortal = true;
-    configPackages = with pkgs; [ xdg-desktop-portal-gtk xdg-desktop-portal-gnome ];
-    config = {
-      common.default = [ "gtk" ];
-      hyprland.default = [ "gtk" "hyprland" ];
-    };
-    extraPortals = [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
+    configPackages = with pkgs; [ niri ];
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal-gnome ];
   };
 }
