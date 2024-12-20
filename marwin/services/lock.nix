521790ff39574ffc62d1b499d4feb863c140c4fe
Marwin Kreuzig <marwin@kreuzig.info>
2024-12-20 17:06:53 +0100

    fix lock screen date display

--- marwin/services/lock.nix
+++ marwin/services/lock.nix
@@ -2,7 +2,8 @@
 {
   services.swayidle = {
     enable = true;
-    events = let lock = "${pkgs.swaylock-effects}/bin/swaylock -e -K --screenshots --clock --datestr '%a, %Y-%m-%d' --indicator --effect-blur 14x5 --fade-in 2 --effect-vignette 0.5:0.5 --ring-color aa0000 --key-hl-color ff0000 --inside-clear-color aa0000 --ring-clear-color ff0000"; in [
+    # the %% in the datestr format are just an escaped % because this string is put inside a systemd service and systemd has its own templating/escaping system
+    events = let lock = "${pkgs.swaylock-effects}/bin/swaylock -e -K --screenshots --clock --datestr \"%%a, %%Y-%%m-%%d\" --indicator --effect-blur 14x5 --fade-in 2 --effect-vignette 0.5:0.5 --ring-color aa0000 --key-hl-color ff0000 --inside-clear-color aa0000 --ring-clear-color ff0000"; in [
       { event = "before-sleep"; command = lock; }
       { event = "lock"; command = lock; }
     ];
