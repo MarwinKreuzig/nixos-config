15cbb8866d0b87cd009fa05f5831ca880286dc93
Marwin Kreuzig <marwin@kreuzig.info>
2025-02-07 12:39:09 +0100

    add option to switch discord command for auto start and set it to discord-canary

--- marwin/default.nix
+++ marwin/default.nix
@@ -1,11 +1,12 @@
-{ host, uses-nvidia, ... }:
-
+{ lib, host, uses-nvidia, pkgs, ... }:
 {
-  home.username = "marwin";
-  home.homeDirectory = "/home/marwin";
-
-  xdg.configFile."nixpkgs/config.nix".text = "{ allowUnfree = true; }";
-
+  options.home.settings = {
+    discord = lib.mkOption {
+      type = lib.types.str;
+      default = "discord";
+      description = "The command used to auto-start discord.";
+    };
+  };
   imports = [
     ./xdg.nix
     ./programs
@@ -14,20 +15,28 @@
     ./services
     ./theme
   ];
+  config = {
+    home.username = "marwin";
+    home.homeDirectory = "/home/marwin";
 
-  home.hyprland.enable = false;
-  home.hyprland.setup = host;
-  home.hyprland.nvidia = uses-nvidia;
+    xdg.configFile."nixpkgs/config.nix".text = "{ allowUnfree = true; }";
 
-  home.niri = {
-    enable = true;
-    setup = host;
-    nvidia = uses-nvidia;
+    home.hyprland.enable = false;
+    home.hyprland.setup = host;
+    home.hyprland.nvidia = uses-nvidia;
+
+    home.niri = {
+      enable = true;
+      setup = host;
+      nvidia = uses-nvidia;
+    };
+
+    home.settings.discord = "discord-canary";
+    
+    # make home-manager manage fonts
+    fonts.fontconfig.enable = true;
+
+    home.stateVersion = "23.05";
+    programs.home-manager.enable = true;
   };
-
-  # make home-manager manage fonts
-  fonts.fontconfig.enable = true;
-
-  home.stateVersion = "23.05";
-  programs.home-manager.enable = true;
 }
