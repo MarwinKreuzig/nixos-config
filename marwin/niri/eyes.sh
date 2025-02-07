37843601627b1aafdec8fad72ac05348006b5ea3
Marwin Kreuzig <marwin@kreuzig.info>
2025-02-07 12:37:41 +0100

    fixed mcsr resize macros

--- marwin/niri/eyes.sh
+++ marwin/niri/eyes.sh
@@ -1,4 +1,5 @@
-niri msg action fullscreen-window
-niri msg action center-column
+niri msg action move-window-to-floating
+niri msg action move-floating-window -y 0
+niri msg action move-floating-window -x 810 -y -7652
 niri msg action set-window-height 16384
 niri msg action set-column-width 300
--- marwin/niri/mapless.sh
+++ marwin/niri/mapless.sh
@@ -1,4 +1,4 @@
-niri msg action fullscreen-window
-niri msg action center-column
+niri msg action move-window-to-floating
+niri msg action move-floating-window -x 810 -y 140
 niri msg action set-window-height 800
 niri msg action set-column-width 300
