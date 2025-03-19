524f8c80151c9165584302d8c6ddef58017c9afa
Marwin Kreuzig <marwin@kreuzig.info>
2025-03-19 17:06:52 +0100

    update flake

--- marwin/programs/alacritty.nix
+++ marwin/programs/alacritty.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  xdg.configFile."alacritty/alacritty.toml".source = "${pkgs.alacritty-theme}/github_light_high_contrast.toml";
+  xdg.configFile."alacritty/alacritty.toml".source = "${pkgs.alacritty-theme}/share/alacritty-theme/github_light_high_contrast.toml";
 
   programs.alacritty = {
     enable = true;
