12d51d5414b77075a7877fe52eaa316d61f90430
Marwin Kreuzig <marwin@kreuzig.info>
2025-05-18 21:14:36 +0200

    pin home manager until nixpkgs updates

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   nixConfig = {
     experimental-features = [ "nix-command" "flakes" ];
     substitutors = [
-      #      "https://hyprland.cachix.org"
       "https://app.cachix.org/cache/jakestanger"
       "https://cache.nixos.org/"
     ];
@@ -13,7 +12,6 @@
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
-      #     "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "jakestanger.cachix.org-1:VWJE7AWNe5/KOEvCQRxoE8UsI2Xs2nHULJ7TEjYm7mM="
     ];
@@ -23,15 +21,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixmaster.url = "nixpkgs/master";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nvim-nix-config.url = "github:MarwinKreuzig/nvim-nix-config";
-    #   hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-    hyprland-contrib = {
-      url = "github:hyprwm/contrib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     pipewire-screenaudio.url = "github:IceDBorn/pipewire-screenaudio";
   };
 
