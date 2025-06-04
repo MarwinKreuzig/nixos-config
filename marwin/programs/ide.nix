4365e49a5081d397970139ff170e81f70ba9e08c
Marwin Kreuzig <marwin@kreuzig.info>
2025-06-04 18:15:50 +0200

    setup npm global installs

--- marwin/programs/ide.nix
+++ marwin/programs/ide.nix
@@ -6,4 +6,11 @@
     jetbrains.idea-ultimate
     jetbrains.gateway
   ];
+
+  # create a path to global install npm binaries to
+  # use with `npm set prefix ~/.npm-global`
+  # see also https://nixos.wiki/wiki/Node.js
+  home.sessionPath = [
+    "$HOME/.npm-global/bin"
+  ];
 }
