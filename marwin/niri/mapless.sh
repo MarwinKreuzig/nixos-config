8cbe628f7e68f0ee8c4bb17bf2094b2ec6c6b6e3
Marwin Kreuzig <marwin@kreuzig.info>
2025-06-11 15:14:46 +0200

    remove useless files

--- marwin/niri/mapless.sh
+++
@@ -1,5 +0,0 @@
-niri msg action move-window-to-floating
-niri msg action move-floating-window -x 810 -y 140
-niri msg action set-window-height 800
-niri msg action set-column-width 300
-niri msg action focus-floating
