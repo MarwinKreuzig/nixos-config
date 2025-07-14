5ea3eff0b8905ec718a091f427e84349303e4840
Marwin Kreuzig <marwin@kreuzig.info>
2025-07-14 13:52:02 +0200

    switch to latest nvidia drivers

--- hosts/common/hardware/nvidia.nix
+++ hosts/common/hardware/nvidia.nix
@@ -3,19 +3,17 @@ if uses-nvidia then {
   boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
 
   hardware.graphics.extraPackages = [ pkgs.libvdpau-va-gl ];
-  environment.variables.VDPAU_DRIVER = "va_gl";
-  environment.variables.LIBVA_DRIVER_NAME = "nvidia";
 
   hardware.nvidia = {
     modesetting.enable = true; # this is required
     powerManagement.enable = true; # this can cause issues if enabled
 
-    open = false;
+    # This is preferred for newer GPUs
+    open = true;
 
     nvidiaSettings = true;
 
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 
   services.xserver.videoDrivers = [ "nvidia" ];
