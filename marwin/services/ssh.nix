10ce6cb113d82f13b24d498cb7981d4483593d7f
Marwin Kreuzig <marwin@kreuzig.info>
2025-07-15 18:41:27 +0200

    add work ssh key config

--- marwin/services/ssh.nix
+++ marwin/services/ssh.nix
@@ -1,4 +1,17 @@
 { ... }:
 {
   services.ssh-agent.enable = true;
+
+  programs.ssh.enable = true;
+  programs.ssh.extraConfig = "
+    Host github
+      Hostname      github.com
+      User git
+      IdentityFile  ~/.ssh/id_ed25519
+
+    Host github-work
+      Hostname      github.com
+      User git
+      IdentityFile  ~/.ssh/id_ed25519_cprosoft
+  ";
 }
