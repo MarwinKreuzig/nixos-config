3e22a38cd53571c8c8a418e67863872c62edf5fd
Marwin Kreuzig <marwin@kreuzig.info>
2025-08-22 12:10:16 +0200

    hack: remove useless jdk overlay and add nvidia vulkan fix for swaync

--- modules/hack/host.nix
+++ modules/hack/host.nix
@@ -1,12 +1,21 @@
 { ... }:
 {
-  nixpkgs.overlays = [
-    (final: prev: {
-      # there's currently a build failure for jdk8
-      jdk8 = prev.jdk8.overrideAttrs {
-        separateDebugInfo = false;
-        __structuredAttrs = false;
-      };
-    })
-  ];
+  # nixpkgs.overlays = [
+  #   (final: prev: {
+  #     # there's currently a build failure for jdk8
+  #     jdk8 = prev.jdk8.overrideAttrs {
+  #       separateDebugInfo = false;
+  #       __structuredAttrs = false;
+  #     };
+  #   })
+  # ];
+  
+  
+  
+  # see
+  # https://github.com/ErikReider/SwayNotificationCenter/issues/619
+  environment.sessionVariables = {
+    GSK_RENDERER = "gl";
+    GTK_DISABLE_VULKAN = 1;
+  };
 }
