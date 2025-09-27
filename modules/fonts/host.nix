d460e57e16afe0e36cc42a258e249224dde451a3
Marwin Kreuzig <marwin@kreuzig.info>
2025-09-27 14:26:14 +0200

    fonts: add more fonts

--- modules/fonts/host.nix
+++ modules/fonts/host.nix
@@ -5,7 +5,11 @@
     packages = with pkgs; [
       font-awesome
       nerd-fonts.fira-code
+      jetbrains-mono
       ubuntu_font_family
+      roboto
+      roboto-mono
+      inter
     ];
   };
 }
