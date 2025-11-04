26f6b939f22e1f2043236795f5bae2a263586574
Marwin Kreuzig <marwin@kreuzig.info>
2025-11-04 09:54:12 +0100

    ssh: make ssh default config explicit to prepare for the removal of the default config

--- modules/ssh/home.nix
+++ modules/ssh/home.nix
@@ -14,4 +14,17 @@
       User git
       IdentityFile  ~/.ssh/id_ed25519_cprosoft
   ";
+  programs.ssh.enableDefaultConfig = false;
+  programs.ssh.matchBlocks."*" = {
+    forwardAgent = false;
+    addKeysToAgent = "no";
+    compression = false;
+    serverAliveInterval = 0;
+    serverAliveCountMax = 3;
+    hashKnownHosts = false;
+    userKnownHostsFile = "~/.ssh/known_hosts";
+    controlMaster = "no";
+    controlPath = "~/.ssh/master-%r@%n:%p";
+    controlPersist = "no";
+  };
 }
