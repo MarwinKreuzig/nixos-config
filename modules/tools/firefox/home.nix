87ee1e5ba5b6158cfed8d3b15b6c79a6c5f98b76
Marwin Kreuzig <marwin@kreuzig.info>
2025-12-22 21:22:26 +0100

    tools/firefox: declaratively configure firefox

--- modules/tools/firefox/home.nix
+++ modules/tools/firefox/home.nix
@@ -1,11 +1,6 @@
 { pkgs, inputs, ... }:
 {
-  home.packages = with pkgs; [
-    (firefox.override {
-      nativeMessagingHosts = [
-        inputs.pipewire-screenaudio.packages.${pkgs.stdenv.hostPlatform.system}.default
-      ];
-    })
+  home.packages = [
     (pkgs.makeDesktopItem {
       name = "firefox-p";
       desktopName = "Firefox Profile";
@@ -13,4 +8,91 @@
       terminal = true;
     })
   ];
+  programs.firefox = {
+    enable = true;
+    nativeMessagingHosts = [
+      inputs.pipewire-screenaudio.packages.${pkgs.stdenv.hostPlatform.system}.default
+    ];
+    languagePacks = [ "de-DE" "en-US" ];
+    profiles = {
+      private = {
+        name = "Private";
+        isDefault = true;
+        id = 0;
+        search = {
+          engines = {
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    { name = "channel"; value = "unstable"; }
+                    { name = "query";   value = "{searchTerms}"; }
+                  ];
+                }
+              ];
+              icon           = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@np" ];
+            };
+
+            "Nix Options" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/options";
+                  params = [
+                    { name = "channel"; value = "unstable"; }
+                    { name = "query";   value = "{searchTerms}"; }
+                  ];
+                }
+              ];
+              icon           = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@no" ];
+            };
+
+            "NixOS Wiki" = {
+              urls = [
+                {
+                  template = "https://wiki.nixos.org/w/index.php";
+                  params = [
+                    { name = "search"; value = "{searchTerms}"; }
+                  ];
+                }
+              ];
+              icon           = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@nw" ];
+            };
+          };
+        };
+      };
+      work = {
+        name = "Work";
+        isDefault = false;
+        id = 1;
+      };
+    };
+    policies = {
+      OfferToSaveLogins = false;
+      DontCheckDefaultBrowser = true;
+      DisablePocket = true;
+      DisableSetDesktopBackground = true;
+      BlockAboutConfig = false;
+      Preferences = {
+        # disable all AI
+        "browser.ml.chat.enabled" = false;
+        "browser.ml.enable" = false;
+        "browser.ml.linkPreview.enabled" = false;
+        "browser.ml.pageAssist.enabled" = false;
+        "browser.ml.smartAssist.enabled" = false;
+        "extensions.ml.enabled" = false;
+        "browser.tabs.groups.smart.enabled" = false;
+        "browser.search.visualSearch.featureGate" = false;
+        "browser.urlbar.quicksuggest.mlEnabled" = false;
+        "places.semanticHistory.featureGate" = false;
+        "sidebar.revamp" = false;
+
+        # enable middle mouse scrolling :D
+        "general.autoScroll" = true;
+      };
+    };
+  };
 }
