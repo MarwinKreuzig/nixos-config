dfd18d7b7e91b410c7d148590a5544a1506965e6
Marwin Kreuzig <marwin@kreuzig.info>
2026-01-22 01:32:21 +0100

    git: add second ssh key to work directory

--- modules/git/home.nix
+++ modules/git/home.nix
@@ -2,10 +2,19 @@
 {
   programs.git = {
     enable = true;
+    includes = [
+      {
+        path = ./work.gitconfig;
+        condition = "gitdir:~/Projects/CProSoft";
+      }
+    ];
     settings = {
       user = {
         name = "Marwin Kreuzig";
         email = "marwin@kreuzig.info";
+      };
+      github = {
+        user = "MarwinKreuzig";
       };
       init.defaultBranch = "main";
     };
