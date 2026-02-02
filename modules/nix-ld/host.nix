94eae2d34366b3d4c888d0980bfc0a89c0bd3f74
Marwin Kreuzig <marwin@kreuzig.info>
2026-02-02 10:27:25 +0100

    nix-ld: add more libraries

--- modules/nix-ld/host.nix
+++ modules/nix-ld/host.nix
@@ -5,6 +5,32 @@
     libraries = options.programs.nix-ld.libraries.default ++ (with pkgs; [
       # dynamic libraries for nix-ld
       libGL
+
+      # needed for FoundryVTT
+      glib
+      nss
+      nspr
+      libx11
+      libxcb
+      libxcomposite
+      libxcursor
+      libxdamage
+      libxext
+      libxfixes
+      libxi
+      libxrender
+      libxtst
+      cups
+      dbus
+      expat
+      libxscrnsaver
+      libxrandr
+      alsa-lib
+      pango
+      cairo
+      at-spi2-atk
+      gtk3
+      gdk-pixbuf
     ]);
   };
 }
