59a11fb3f51301408206b7bc1e982507e7d951c5
Marwin Kreuzig <marwin@kreuzig.info>
2026-03-11 17:04:13 +0100

    modules/hack: cleanup

--- modules/hack/host.nix
+++ modules/hack/host.nix
@@ -1,12 +1,5 @@
 { lib, config, ... }:
 {
-  # nixpkgs.overlays = [
-  #   (final: prev: {
-  #     # there's currently a build failure for jdk8
-  #     jdk8 = prev.jdk8.overrideAttrs {
-  #       separateDebugInfo = false;
-  #       __structuredAttrs = false;
-  #     };
-  #   })
-  # ];
+  nixpkgs.overlays = [
+  ];
 }
