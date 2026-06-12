ecc427156e14d172c5d56a164b4846224233ca82
Marwin Kreuzig <marwin@kreuzig.info>
2026-06-12 18:53:32 +0200

    hack: make bitwarden derivation evaluate

--- modules/hack/host.nix
+++ modules/hack/host.nix
@@ -1,14 +1,18 @@
-{ lib, config, ... }:
+{ lib, config, inputs, ... }:
 {
   nixpkgs.overlays = [
-    (_: prev: {
+    (final: prev: {
       openldap = prev.openldap.overrideAttrs {
         doCheck = !prev.stdenv.hostPlatform.isi686;
       };
+
+      bitwarden-desktop = prev.bitwarden-desktop.override { electron_39 = final.electron_39-bin; };
     })
+    inputs.niri-tearing.overlays.default
   ];
 
 
   nixpkgs.config.permittedInsecurePackages = [
+    "electron-39.8.10"
   ];
 }
