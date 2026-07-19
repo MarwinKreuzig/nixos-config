bf173db3059d8418ec55721dc282cf33cf150fdd
Marwin Kreuzig <marwin@kreuzig.info>
2026-07-19 13:30:52 +0200

    hack: remove obsolete openldap fix, add links to relevant issues

--- modules/hack/host.nix
+++ modules/hack/host.nix
@@ -1,18 +1,17 @@
 { lib, config, inputs, ... }:
 {
   nixpkgs.overlays = [
+    # https://github.com/NixOS/nixpkgs/issues/526914
     (final: prev: {
-      openldap = prev.openldap.overrideAttrs {
-        doCheck = !prev.stdenv.hostPlatform.isi686;
-      };
-
       bitwarden-desktop = prev.bitwarden-desktop.override { electron_39 = final.electron_39-bin; };
     })
+    # https://github.com/niri-wm/niri/issues/844
     inputs.niri-tearing.overlays.default
   ];
 
 
   nixpkgs.config.permittedInsecurePackages = [
+    # https://github.com/NixOS/nixpkgs/issues/526914
     "electron-39.8.10"
   ];
 }
